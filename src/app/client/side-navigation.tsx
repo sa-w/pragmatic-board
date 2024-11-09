88216593f80977a130f74e8bc494aea9f420d8f2
Alex Reardon <areardon@atlassian.com>
2024-11-09 16:08:35 +1100

    wip

--- src/app/client/side-navigation.tsx
+++ src/app/client/side-navigation.tsx
@@ -9,14 +9,19 @@ export function SideNavigation() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
-    <nav className="flex flex-col bg-slate-200">
-      <h2 className="bg-purple-50 p-2">🤹 Automatic scrolling playground</h2>
-      <Link
-        href="/example/list"
-        className={`p-2 ${pathname === '/example/list' ? 'bg-blue-100' : ''}`}
-      >
-        List example
-      </Link>
+    <nav className="flex flex-col justify-stretch bg-slate-200">
+      <header className="sticky">
+        <h2 className="bg-purple-50 p-2">🤹 Automatic scrolling playground</h2>
+      </header>
+      <section className="flex grow flex-col overflow-y-auto">
+        <Link
+          href="/example/list"
+          className={`p-2 ${pathname === '/example/list' ? 'bg-blue-100' : ''}`}
+        >
+          List example
+        </Link>
+      </section>
+      <footer className="sticky">Used by Trello</footer>
     </nav>
   );
 }
--- src/app/example/list/list.tsx
+++ src/app/example/list/list.tsx
@@ -50,7 +50,7 @@ type TCardState =
     }
   | {
       type: 'is-over';
-      closestEdge: Edge;
+      // closestEdge: Edge;
     }
   | {
       type: 'preview';
@@ -61,8 +61,10 @@ type TCardState =
 const idle: TCardState = { type: 'idle' };
 
 const stateStyles: { [Key in TCardState['type']]?: string } = {
+  idle: 'bg-purple-100',
   dragging: 'bg-slate-100',
   preview: 'bg-blue-100',
+  'is-over': 'bg-red-300',
 };
 
 function Card({ card }: { card: TCard }) {
@@ -96,14 +98,23 @@ function Card({ card }: { card: TCard }) {
           setState(idle);
         },
       }),
+      dropTargetForElements({
+        element,
+        onDragEnter() {
+          setState({ type: 'is-over' });
+        },
+        onDragLeave() {
+          setState(idle);
+        },
+        onDrop() {
+          setState(idle);
+        },
+      }),
     );
   }, []);
   return (
     <>
-      <div
-        ref={ref}
-        className={`rounded border p-2 ${card.backgroundColor} ${stateStyles[state.type]}`}
-      >
+      <div ref={ref} className={`rounded border p-2 ${stateStyles[state.type]}`}>
         <div className="flex flex-row items-center gap-2">
           <div className={`${card.avatarColor} rounded-full p-2`}>{card.icon}</div>
           <div>{card.description}</div>
